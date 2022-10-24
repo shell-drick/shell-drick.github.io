30c5232172ead1af384f356cfe7e6ed6f3626d04
Andrew Sheldrick <ajsheld12@gmail.com>
2022-10-24 02:21:07 -0400

    fix

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-
-const quotes = fs.readFileSync("quotes.json").split('/\r?\n/');
-
 function popupQuote() {
+    let quotes = readFileSync("quotes.json").split('/\r?\n/');
     let q = quotes[Math.floor(quotes.length*Math.random())];
     alert(`${q.body}\r\n\t-${q.author}`);
 }
